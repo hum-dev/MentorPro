9f3e40799a8b246c80c5098d2956d21b99487b8b
hum-dev <humphreymuriungi@outlook.com>
2023-06-25 10:59:26 +0300

    handled mentor and mentee registration

--- mentorpro/src/components/Mentee/MenteeData.jsx
+++ mentorpro/src/components/Mentee/MenteeData.jsx
@@ -1,5 +1,13 @@
 
 import './MenteeData.css';
+import { useForm } from 'react-hook-form';
+import { yupResolver } from '@hookform/resolvers/yup';
+import * as yup from 'yup';
+import {  useNavigate } from 'react-router-dom';
+// import Axios from 'axios';
+import { toast } from 'react-toastify';
+import "react-toastify/dist/ReactToastify.css";
+
 
 const genderOptions = [
   { label: 'Male', value: 'male' },
@@ -7,18 +15,57 @@ const genderOptions = [
   { label: 'Other', value: 'other' }
 ];
 
-const Mentee= () => {
+function Mentee () {
+  const navigate = useNavigate();
+  const Schema = yup.object().shape({
+    full_name: yup.string().required("name is required"),
+    email: yup.string().email("Invalid email").required("Email is required"),
+    phone_number: yup
+      .string()
+      .matches(
+        /^\+(?:[0-9] ?){6,14}[0-9]$/,
+        "Phone number must be a valid international phone number"
+      )
+      .required("Phone number is required"),
+      
+    bio: yup.string().required("Bio is required"),
+    tech_field: yup.string().required("Tech field is required"),
+    expectation: yup.string().required("Expectation is required"),
+
+      
+  });
+
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm({
+    resolver: yupResolver(Schema),
+  });
 
   
-  const handleSubmit = (e) => {
-    e.preventDefault();
-
+  const sendDataToServer = async (data) => {
+    
+    const response = await fetch("http://localhost:8081/mentee/create", {
+      method: "POST",
+      body: JSON.stringify(data),
+      headers: {
+        "Content-Type": "application/json",
+      }
+    })
+    console.log(response.status)
+    if(response.status === 200) {
+      toast.success("registerd  successfully");
+      navigate('/Home')
+    } else {
+      toast.error("Something went wrong");
+    }
     
    
   };
 
   return (
-    <form onSubmit={handleSubmit} className='form-container'>
+    <form onSubmit={handleSubmit (sendDataToServer)} className='form-container'>
          <p className="title">Signup to be a Mentee</p>
       <div className='form-group'>
         <label htmlFor="full_name">Full Name</label>
@@ -26,8 +73,9 @@ const Mentee= () => {
           type="text"
           id="full_name"
           name="full_name"
-          
+          {...register('full_name')}
         />
+        {errors.full_name && <p>{errors.full_name.message}</p>}
       </div>
 
       <div className='form-group'> 
@@ -35,7 +83,7 @@ const Mentee= () => {
         <select
           id="gender"
           name="gender"
-         
+         {...register("gender")}
         >
           <option value="">Select</option>
           {genderOptions.map((option) => (
@@ -51,9 +99,11 @@ const Mentee= () => {
         <input
           type="tel"
           id="phone_number"
-          name="phonenumber"
+          name="phone_number"
           placeholder='Phone Number'
+          {...register('phone_number')}
         />
+        {errors.phone_number && <p>{errors.phone_number.message}</p>}
       </div>
 
       <div className='form-group'>
@@ -62,9 +112,10 @@ const Mentee= () => {
           type="email"
           id="email"
           name="email"
-          
           placeholder='email'
+          {...register('email')}
         />
+        {errors.email && <p>{errors.email.message}</p>}
       </div>
 
       <div className='form-group'>
@@ -73,9 +124,10 @@ const Mentee= () => {
           type="text"
           id="bio"
           name="bio"
-         
           placeholder='LinknedIn profile'
+          {...register('bio')}
         />
+        {errors.bio && <p>{errors.bio.message}</p>}
       </div>
 
       <div className='form-group'>
@@ -83,10 +135,12 @@ const Mentee= () => {
         <input
           type="text"
           id="techfield"
-          name="techfield"
+          name="tech_field"
           placeholder='field of interest'
+          {...register('tech_field')}
         
         />
+        {errors.tech_field && <p>{errors.tech_field.message}</p>}
       </div>
 
       <div className="form-group">
@@ -95,13 +149,14 @@ const Mentee= () => {
           id="exp"
           name="expectation"
             placeholder="expectation"
-          
+          {...register('expectation')}
         ></textarea>
+        {errors.expectation && <p>{errors.expectation.message}</p>}
       </div>
 
       <button className='sign' type="submit"><span>Submit</span></button>
     </form>
   );
-};
+}
 
 export default Mentee;
--- mentorpro/src/components/Mentor/MentorsData.jsx
+++ mentorpro/src/components/Mentor/MentorsData.jsx
@@ -1,5 +1,12 @@
 
 import './MentorsData.css';
+import { useForm } from 'react-hook-form';
+import { yupResolver } from '@hookform/resolvers/yup';
+import * as yup from 'yup';
+import {  useNavigate } from 'react-router-dom';
+// import Axios from 'axios';
+import { toast } from 'react-toastify';
+import "react-toastify/dist/ReactToastify.css";
 
 const genderOptions = [
   { label: 'Male', value: 'male' },
@@ -7,18 +14,64 @@ const genderOptions = [
   { label: 'Other', value: 'other' }
 ];
 
-const Mentor= () => {
+function Mentor () {
+  const navigate = useNavigate();
+  const Schema = yup.object().shape({
+    full_name: yup.string().required("name is required"),
+    email: yup.string().email("Invalid email").required("Email is required"),
+    phone_number: yup
+      .string()
+      .matches(
+        /^\+(?:[0-9] ?){6,14}[0-9]$/,
+        "Phone number must be a valid international phone number"
+      )
+      .required("Phone number is required"),
+      
+    bio: yup.string().required("Bio is required"),
+    tech_field: yup.string().required("Tech field is required"),
 
+      
+  });
+
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm({
+    resolver: yupResolver(Schema),
+  });
   
-  const handleSubmit = (e) => {
-    e.preventDefault();
+  const sendDataToServer = async (data) => {
+    // Axios.post("http://localhost:8081/mentor/create" , data)
+    // .then((response) => { 
+    //   response.data.message && toast.message(response.data.message);
 
-    
-   
+    //   navigate('/Home')
+    // })
+    // .catch(({response}) => { 
+    //   const error = response.data.message;
+    //   toast.error(error);
+     
+    // });
+
+
+     const response = await fetch("http://localhost:8081/mentor/create", {
+        method: "POST",
+        body: JSON.stringify(data),
+        headers: {
+          "Content-Type": "application/json",
+        }
+      })
+      console.log(response.status)
+      if(response.status === 200) {
+        toast.success("registerd  successfully");
+        navigate('/Home')
+      } else {
+        toast.error("Something went wrong");
+      }
   };
-
   return (
-    <form onSubmit={handleSubmit} className='form-container'>
+    <form onSubmit={handleSubmit (sendDataToServer)} className='form-container'>
          <p className="title">Sign up as a Mentor</p>
       <div className='form-group'>
         <label htmlFor="full_name">Full Name</label>
@@ -26,8 +79,9 @@ const Mentor= () => {
           type="text"
           id="full_name"
           name="full_name"
-          
+          {...register("full_name")}
         />
+        {errors.full_name && toast.error(errors.full_name?.message)}
       </div>
 
       <div className='form-group'> 
@@ -35,7 +89,7 @@ const Mentor= () => {
         <select
           id="gender"
           name="gender"
-         
+          {...register("gender")}
         >
           <option value="">Select</option>
           {genderOptions.map((option) => (
@@ -51,9 +105,11 @@ const Mentor= () => {
         <input
           type="tel"
           id="phone_number"
-          name="phonenumber"
+          name="phone_number"
           placeholder='Phone Number'
+          {...register("phone_number")}
         />
+        {errors.phone_number && toast.error(errors.phone_number?.message)}
       </div>
 
       <div className='form-group'>
@@ -62,9 +118,10 @@ const Mentor= () => {
           type="email"
           id="email"
           name="email"
-          
           placeholder='email'
+          {...register("email")}
         />
+        {errors.email && toast.error(errors.email?.message)}
       </div>
 
       <div className='form-group'>
@@ -73,9 +130,10 @@ const Mentor= () => {
           type="text"
           id="bio"
           name="bio"
-         
           placeholder='LinknedIn profile'
+          {...register("bio")}
         />
+        {errors.bio && toast.error(errors.bio?.message)}
       </div>
 
       <div className='form-group'>
@@ -84,13 +142,14 @@ const Mentor= () => {
           type="text"
           id="tech_field"
           name="techfield"
-        
+        {...register("tech_field")}
         />
+        {errors.tech_field && toast.error(errors.tech_field?.message)}
       </div>
 
       <button className='sign' type="submit"><span>Submit</span></button>
     </form>
   );
-};
+}
 
 export default Mentor;
